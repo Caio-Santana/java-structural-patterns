3096151fcce149564c1dadd4d6c55c4d780203b9
Caio-Santana <caio.santana@live.com>
2022-10-16 14:25:04 -0300

    Initial commit

---
+++ src/main/java/com/caio/structuralpatterns/StructuralPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.caio.structuralpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuralPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructuralPatternsApplication.class, args);
+	}
+
+}
